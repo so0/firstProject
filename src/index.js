fa82b3aee59f1dfc1c5cee33fc67f8929ce29cf6
so0 <syngshm@gmail.com>
2019-01-29 19:45:26 +0900

    react project setting

---
+++ src/index.js
@@ -0,0 +1,12 @@
+	
+console.log('Set up is continuing...');
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+const title = 'React Webpack Babel Setup';
+ 
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('root')
+);
+ 
